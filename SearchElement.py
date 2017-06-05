7b492989759c3c3b7298b04f0a470d70644484e9
Lucas <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2017-06-04 21:12:17 -0300

    Encontra minimo

--- SearchElement.py
+++ SearchElement.py
@@ -6,3 +6,11 @@ def maximo(array):
 		if (i > maxElement):
 			maxElement = i
 	return maxElement
+
+def minimo(array):
+	minElement = array[0]
+	
+	for i in array:
+		if (i < minElement):
+			minElement = i
+	return minElement
